0ee831129534fad21a0fb2aa31efa535b91db1ce
Asif Iqbal <aasif2727@yahoo.com>
2018-08-10 13:14:52 +0530

    review page fix

--- app/scripts/controllers/reviewController.js
+++ app/scripts/controllers/reviewController.js
@@ -18,4 +18,40 @@ angular.module('indiaTours').controller("reviewController", function ($scope,$lo
             indiaTourService.updateItineraryHits(_state,_code);
         }
     };
+
+    $scope.bindIcon = function(){
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'adventure'){
+            return 'icon_set_1_icon-3';
+        }
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'craft'){
+            return 'icon_set_1_icon-44';
+        }   
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'beach'){
+            return 'icon_set_1_icon-8';
+        }     
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'camping'){
+            return 'icon_set_1_icon-37';
+        }        
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'city'){
+            return 'icon_set_1_icon-40';
+        }    
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'cultural'){
+            return 'icon_set_1_icon-1';
+        }  
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'heritage'){
+            return 'icon_set_1_icon-4';
+        }  
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'temple'){
+            return 'icon_set_1_icon-1';
+        }    
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'tribal'){
+            return 'icon_set_1_icon-30';
+        }  
+        if($scope.categoryParam != undefined && $scope.categoryParam == 'wildlife'){
+            return 'icon_set_1_icon-30';
+        }  
+        else{
+            return 'icon_set_1_icon-30';
+        }
+    };
 });
