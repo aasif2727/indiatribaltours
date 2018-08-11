35dd51000a1fe9548223e4953b2829b04c15516a
Asif Iqbal <aasif2727@yahoo.com>
2018-08-11 19:53:46 +0530

    rev-slider updated

--- app/scripts/rev-slider.js
+++ app/scripts/rev-slider.js
@@ -1,74 +1,70 @@
 var tpj = jQuery;
-
-var revapi54;
+var revapi22;
 tpj(document).ready(function () {
-    if (tpj("#rev_slider_54_1").revolution == undefined) {
-        revslider_showDoubleJqueryError("#rev_slider_54_1");
+    if (tpj("#rev_slider_22_1").revolution == undefined) {
+        revslider_showDoubleJqueryError("#rev_slider_22_1");
     } else {
-        revapi54 = tpj("#rev_slider_54_1").show().revolution({
+        revapi22 = tpj("#rev_slider_22_1").show().revolution({
             sliderType: "standard",
             jsFileLocation: "rev-slider-files/js/",
-            sliderLayout: "fullwidth",
+            sliderLayout: "auto",
             dottedOverlay: "none",
-            delay: 9000,
+            delay: 6000,
             navigation: {
-                    keyboardNavigation:"off",
-                    keyboard_direction: "horizontal",
-                    mouseScrollNavigation:"off",
-                     mouseScrollReverse:"default",
-                    onHoverStop:"off",
-                    touch:{
-                        touchenabled:"on",
-                        touchOnDesktop:"off",
-                        swipe_threshold: 75,
-                        swipe_min_touches: 50,
-                        swipe_direction: "horizontal",
-                        drag_block_vertical: false
-                    }
-                    ,
-                    arrows: {
-                        style:"uranus",
-                        enable:true,
-                        hide_onmobile:true,
-                        hide_under:778,
-                        hide_onleave:true,
-                        hide_delay:200,
-                        hide_delay_mobile:1200,
-                        tmp:'',
-                        left: {
-                            h_align:"left",
-                            v_align:"center",
-                            h_offset:20,
-                            v_offset:0
-                        },
-                        right: {
-                            h_align:"right",
-                            v_align:"center",
-                            h_offset:20,
-                            v_offset:0
-                        }
-                    }
+                keyboardNavigation: "off",
+                keyboard_direction: "horizontal",
+                mouseScrollNavigation: "off",
+                mouseScrollReverse: "default",
+                onHoverStop: "off",
+                touch: {
+                    touchenabled: "on",
+                    touchOnDesktop: "off",
+                    swipe_threshold: 75,
+                    swipe_min_touches: 1,
+                    swipe_direction: "horizontal",
+                    drag_block_vertical: false
                 },
-            responsiveLevels: [1240, 1024, 778, 480],
-            visibilityLevels: [1240, 1024, 778, 480],
+                bullets: {
+                    enable: true,
+                    hide_onmobile: true,
+                    hide_under: 600,
+                    style: "hermes",
+                    hide_onleave: true,
+                    hide_delay: 200,
+                    hide_delay_mobile: 1200,
+                    direction: "vertical",
+                    h_align: "right",
+                    v_align: "center",
+                    h_offset: 30,
+                    v_offset: 0,
+                    space: 5,
+                    tmp: ''
+                }
+            },
+            viewPort: {
+                enable: true,
+                outof: "pause",
+                visible_area: "80%",
+                presize: false
+            },
+            responsiveLevels: [1240, 1024, 778, 778],
+            visibilityLevels: [1240, 1024, 778, 778],
             gridwidth: [1240, 1024, 778, 480],
-            gridheight: [700, 550, 860, 480],
+            gridheight: [700, 600, 500, 400],
             lazyType: "none",
             parallax: {
                 type: "mouse",
                 origo: "slidercenter",
                 speed: 2000,
                 levels: [2, 3, 4, 5, 6, 7, 12, 16, 10, 50, 47, 48, 49, 50, 51, 55],
-                disable_onmobile: "on"
             },
             shadow: 0,
             spinner: "off",
-            stopLoop: "on",
-            stopAfterLoops: 0,
-            stopAtSlide: 1,
+            stopLoop: "off",
+            stopAfterLoops: -1,
+            stopAtSlide: -1,
             shuffle: "off",
             autoHeight: "off",
-            disableProgressBar: "on",
             hideThumbsOnMobile: "off",
             hideSliderAtLimit: 0,
             hideCaptionAtLimit: 0,
@@ -81,5 +77,4 @@ tpj(document).ready(function () {
             }
         });
     }
-}); 
-	
+}); /*ready*/
